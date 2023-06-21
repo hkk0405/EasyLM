617eaf9dfa6e4613fa9b859e8fa9e63c0fe3f018
donggyukimc <donggyukimc@gmail.com>
2023-06-21 06:18:09 +0000

    rm script

--- run_train.sh
+++
@@ -1,8 +0,0 @@
-python3 -m EasyLM.models.llama.llama_train \
-    --tokenizer_path='EleutherAI/polyglot-ko-1.3b' \
-    --data_path='output/features' \
-    --output_dir='output' \
-    --dtype='fp16' \
-    --save_model_freq=1000 \
-    --save_milestone_freq=1000 \
-    --load_llama_config='tiny'
